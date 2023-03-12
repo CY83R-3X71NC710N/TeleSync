a4206ecde8a0d7b4781dc7b89f048b7523b643c6
Gavin Klibowitz <103222783+CY83R-3X71NC710N@users.noreply.github.com>
2023-03-12 13:36:43 -0500

    Update download.ps1

--- download.ps1
+++ download.ps1
@@ -1,3 +1,3 @@
-# Powershell script to download files uploaded to telegram
+# Powershell script to download files uploaded to telegram in the download script select the channel that contains the backup from the date you want to restore from.
 pip3 install -U telegram-upload
 telegram-download --interactive
