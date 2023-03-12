a80371106ae1a48ce7faeeff1577cc283099c983
Gavin Klibowitz <103222783+CY83R-3X71NC710N@users.noreply.github.com>
2023-03-12 12:48:16 -0500

    Update and rename upload.py to upload.ps1

---
+++ upload.ps1
@@ -0,0 +1,3 @@
+# powershell script to upload files inside the encrypted_zips directory one by one to telegram individually using a loop when running for the first time it will ask you to give your tokens
+pip3 install -U telegram-upload
+telegram-upload
