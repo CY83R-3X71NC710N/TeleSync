b9c25438419f46b56261c29907a54f281bf4f92d
Gavin Klibowitz <103222783+CY83R-3X71NC710N@users.noreply.github.com>
2023-03-12 14:50:35 -0500

    Update download.py

--- download.py
+++ download.py
@@ -1,4 +1,12 @@
+import os
 import subprocess
+
+# Check if telegram_downloads directory exists, if not create it
+if not os.path.exists('telegram_downloads'):
+    os.mkdir('telegram_downloads')
+
+# Switch to telegram_downloads directory
+os.chdir('telegram_downloads')
 
 # Run the telegram-download --interactive command
 subprocess.run(['telegram-download', '--interactive'])
